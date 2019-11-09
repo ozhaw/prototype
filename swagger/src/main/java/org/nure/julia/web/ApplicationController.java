1f8331be1ab43c2e54431ff40028957023e39b47
ozhaw <54807759+ozhaw@users.noreply.github.com>
2019-11-09 14:57:22 +0200

    Cors config

--- swagger/src/main/java/org/nure/julia/web/ApplicationController.java
+++ swagger/src/main/java/org/nure/julia/web/ApplicationController.java
@@ -11,6 +11,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
 @RestController
-@CrossOrigin
+@CrossOrigin(exposedHeaders = {"SecurityToken"})
 public @interface ApplicationController {
 }
