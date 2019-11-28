809175adbfe8ba08437474c9f0675e0d595f3666
ozhaw <alexzanderzharkov@gmail.com>
2019-11-28 15:03:07 +0200

    Revert "123"
    This reverts commit ab0a475b0e1c7edf38ad3dac6edacb0101612962.

--- config-server/src/main/java/org/nure/julia/ConfigServerBootApplication.java
+++ config-server/src/main/java/org/nure/julia/ConfigServerBootApplication.java
@@ -2,14 +2,10 @@ package org.nure.julia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableDiscoveryClient
-@EnableEurekaClient
 public class ConfigServerBootApplication {
 
     public static void main(String[] args) {
